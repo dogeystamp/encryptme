5dba3f8675c4d60a9231fdd9eac533dd863c9ee1
dogeystamp <dogeystamp@disroot.org>
2023-01-28 16:08:13 -0500

    made header a template
    encryptme title is now a link to home too

---
+++ scripts/header-template.js
@@ -0,0 +1,8 @@
+header = document.createElement("div");
+header.classList.add("page-header");
+
+header.innerHTML = `
+<a href="index.html"><h1>encryptme</h1></a>
+`
+
+document.body.appendChild(header);
