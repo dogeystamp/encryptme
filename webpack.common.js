df9630ec1e75d4cfbe2b14635a87d9a4bc4331be
dogeystamp <dogeystamp@disroot.org>
2023-02-09 22:03:06 -0500

    webpack: added sitemap plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,20 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const pages = [
 	{
 		id: "index",
 		desc: "Easy to use and simple online tools for encryption and decryption.",
+		changefreq: "weekly",
+		priority: 1.0,
 	},
 	{
 		id: "aes",
 		desc: "Secure and simple tool for AES, with control over all advanced options like key size, salt, AES mode, and others.",
+		changefreq: "weekly",
+		priority: 0.7,
 	},
 ];
 
@@ -33,6 +38,19 @@ module.exports = {
 			filename: "[contenthash].css",
 			chunkFilename: "[id].[contenthash].css",
 		}),
+		new SitemapPlugin({
+			base: "https://encryptme.net",
+			paths: pages.map(
+				(page) => ({
+					path: `/${page.id}.html`,
+					changefreq: page.changefreq,
+					priority: page.priority,
+				})
+			),
+			options: {
+				lastmod: true,
+			},
+		})
 	].concat(
 		pages.map(
 			(page) =>
